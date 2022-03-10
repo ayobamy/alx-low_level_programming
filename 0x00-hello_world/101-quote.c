db2d1af84a34d6c2db920e30ead5093a3c0840bd
ayobamy <ayobamyahmed@gmail.com>
2022-03-10 06:40:09 -0800

    made changes to the 101-quote.c file

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-/*
- * main - Entry point
+/**
+ * main - main block
  * Return: 1
  */
 int main(void)
@@ -12,7 +12,7 @@ int main(void)
 	s = strlen("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
 
 	write(2,
-		"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-		s);
+	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+	      s);
 	return (1);
 }
