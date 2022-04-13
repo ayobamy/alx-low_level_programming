bf79da625862ab1a22bc02c68b354b45cb6cdf53
ayobamy <ayobamyahmed@gmail.com>
2022-04-13 13:50:41 -0700

    a function that executes a function

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -1,7 +1,7 @@
 #include "function_pointers.h"
 
 /**
- * array_iterator - a function that executes a function given as a parameter 
+ * array_iterator - a function that executes a function given as a parameter
  * on each element of an array
  * @array: The array.
  * @size: The size of array.
