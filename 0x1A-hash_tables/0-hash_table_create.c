89d1e421d172c863f8d8d91321f7a6eab3517e9b
ayobamy <ayobamyahmed@gmail.com>
2022-07-01 16:31:31 -0700

    modified

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -18,8 +18,10 @@ hash_table_t *hash_table_create(unsigned long int size)
 
 	ht->size = size;
 	ht->array = malloc(sizeof(hash_node_t *) * size);
+
 	if (ht->array == NULL)
 		return (NULL);
+
 	for (i = 0; i < size; i++)
 		ht->array[i] = NULL;
 
