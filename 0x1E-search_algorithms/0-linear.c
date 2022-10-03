63e87db3f677c3789033b8df0aae94c49a187453
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-03 07:40:46 +0000

    [feat]: linear search in an array

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,15 @@
+#include "search_algos.h"
+
+/**
+ * linear_search - searches for a value in an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: the index of @value or -1 if not found or if @array is NULL
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	
+}
