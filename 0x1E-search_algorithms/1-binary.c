b95c01902d480291d1f4888afccd71716b87c149
ayobamy <ayobamyahmed@gmail.com>
2022-10-03 16:47:59 -0700

    [fix]: fixed betty error

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -11,40 +11,40 @@
 
 int binary_search(int *array, size_t size, int value)
 {
-    size_t start, end, mid, i;
+	size_t start, end, mid, i;
 
-    start = 0;
-    end = size - 1;
+	start = 0;
+	end = size - 1;
 
-    if (array == NULL)
+	if (array == NULL)
 	{
 		return (-1);
 	}
 
-    while (start <= end)
-    {
-
-        printf("Searching in array: ");
+	while (start <= end)
+	{
+		printf("Searching in array: ");
 		for (i = start; i < end; i++)
-        {
-            printf("%d, ", array[i]);
-        }
+		{
+			printf("%d, ", array[i]);
+		}
 		printf("%d\n", array[i]);
 
-        mid = (start + end) / 2;
+		mid = (start + end) / 2;
 
-        if (array[mid] == value)
-            return (mid);
-        
-        if (array[mid] < value)
-        {
-            start = mid + 1;
-        }
-        else
-        {
-            end = mid - 1;
-        }
-    }
+		if (array[mid] == value)
+			return (mid);
+
+		if (array[mid] < value)
+		{
+			start = mid + 1;
+		}
+		else
+		{
+			end = mid - 1;
+		}
+
+	}
 
 	return (-1);
 }
