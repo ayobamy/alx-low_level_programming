e998fcc39fd7ab194325c15ec3d4912131f3390a
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-04 17:16:48 +0000

    [feat]: jump search algorithm

---
+++ 0x1E-search_algorithms/100-jump.c
@@ -0,0 +1,23 @@
+#include "search_algos.h"
+
+/**
+ * jump_search - searches for a value in an array of integers
+ * @array: array of integers
+ * @size: size of the array
+ * @value: value to search for
+ *
+ * Return: the index of @value or -1 if not found or if @array is NULL
+ */
+
+int jump_search(int *array, size_t size, int value)
+{
+	size_t i, jump;
+	jump = sqrt(size);
+
+	if (array == NULL)
+		return (-1);
+
+	
+
+	return (-1);
+}
