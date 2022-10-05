d1a7292cfbc6d845828cf6cac020bd4e3541fb19
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-05 19:14:32 +0000

    [update]: updated jump search algorithm

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -18,9 +18,7 @@ int jump_search(int *array, size_t size, int value)
 	if (array == NULL || size == 0)
 		return (-1);
 
-	i = shift = 0;
-
-	while (shift < size && array[shift] < value)
+	for (i = shift = 0; shift < size && array[shift] < value;)
 	{
 		printf("Value checked array[%ld] = [%d]\n", shift, array[shift]);
 		i = shift;
