a98d69b63ed2007c4dd5198b7ca15f21dbb8a5e6
Ahmed A. Olawale <ayobamyahmed@gmail.com>
2022-10-07 01:05:07 +0000

    [feat]: added prototype to header file

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -13,5 +13,6 @@ int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int _binary_searching(int *array, size_t start, size_t end, int value);
+int advanced_binary(int *array, size_t size, int value);
 
 #endif /* __SEARCH_ALGOS__ */
